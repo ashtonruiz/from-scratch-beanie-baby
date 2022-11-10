99ebfa9877e8f7ceece3152f200c6a5b708bd68e
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-09 18:17:44 -0800

    display babies function working

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ import { fetchBabies, fetchSigns } from './fetch.utils.js';
 import { renderBabyElement, renderAstroOption } from './render_utils.js';
 
 /* Get DOM Elements */
-const babyList = document.getElementById('baby-list');
+const babyListEl = document.getElementById('baby-list');
 
 /* State */
+let babyList = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await fetchBabies('Pisces');
+    babyList = response.data;
+    displayBabies();
+});
 
 /* Display Functions */
+function displayBabies() {
+    for (let baby of babyList) {
+        let babyEl = renderBabyElement(baby);
+        babyListEl.append(babyEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
 console.log(fetchBabies('Pisces'));
 console.log(fetchSigns());
-
-babyList.append(renderBabyElement({ title: 'Fredrick', astroSign: 'big snake' }));
