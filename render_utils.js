d28fa3fd7b89efcd0aeb629d27812444412d9ffd
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-10 17:29:38 -0800

    background images working now

--- render_utils.js
+++ render_utils.js
@@ -1,17 +1,22 @@
 export function renderBabyElement(baby) {
-    let babyBoxEl = document.createElement('li');
+    let babyBoxEl = document.createElement('div');
     let babyNameEl = document.createElement('h2');
     let babyReleaseYear = document.createElement('p');
     let babySignEl = document.createElement('p');
 
     babyNameEl.textContent = baby.title;
+    babyNameEl.classList.add('baby-box-text');
     babyReleaseYear.textContent = `Released ${baby.releaseYear}`;
+    babyReleaseYear.classList.add('baby-box-text');
     babySignEl.textContent = baby.astroSign;
     if (baby.astroSign === null) {
         babySignEl.textContent = 'no sign';
     }
+    babySignEl.classList.add('baby-box-text');
+
     babyBoxEl.append(babyNameEl, babyReleaseYear, babySignEl);
     babyBoxEl.classList.add('baby-box');
+    babyBoxEl.style.backgroundImage = `url(${baby.image})`;
     return babyBoxEl;
 }
 
